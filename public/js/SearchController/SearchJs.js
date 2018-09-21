bfe16016631151d1c300fdd7b52a84db95b848a9
baschingz <36448854+baschingz@users.noreply.github.com>
2018-09-21 16:25:29 +0700

    commit

--- public/js/SearchController/SearchJs.js
+++ public/js/SearchController/SearchJs.js
@@ -1,22 +1,40 @@
 app.controller('SearchController', function ($scope, $http) {
     // get master data
-    
+
     $scope.init = function (page) {
         page = page || null;
         path = base_path + '/getselect';
         // debugger;
         $http.get(path).then(function (response) {
             // debugger;
-            $scope.location = response.data[0];
-            $scope.cost = response.data[1];
-            $scope.service = response.data[2];
-            $scope.typeproject = response.data[3];
+            try {
+               
+            }
+            catch(err) {
+                console.log(err.message); 
+            }
         }, function (response) {
             // debugger;
-            alert('fail');
+            console.log('fail');
         });
     }
 
+    $scope.searching = function(request){
+        var req = {
+            params:request,
+            url:base_path+'getbysearch'
+        }
+        $http.post(req).then(function(response){
+            try {
+                $scope.photographerList = response.data;
+            }
+            catch(err) {
+                console.log(err.message); 
+            }
+        },function(response){
+            console.log('error');
+        });
+    }
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('getselect', 'SearchController@getSelect');
 
+Route::post('getbysearch','PhotographerController@getPhotographerByDate');
+
