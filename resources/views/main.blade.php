aaa5b7a329b8d5a3969f81ccc4e0caf5f50cef25
jiidapa <jidapa.p@kkumail.com>
2018-09-21 16:38:12 +0700

    commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,15 +5,24 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Krixxi</title>
+
     @yield('import')
+
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-grid.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-reboot.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
+    <link rel="icon" href="{{asset('../images/icon/Icon-20.png')}}" type="image/x-icon"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500|Quicksand:300,400,500" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
     <script src = "{{asset('js/angularjs.min.js')}}" ></script>
     <script src="{{asset('js/jquery.js')}}"></script>
     <script type="text/javascript">
         var base_path = "{{url('/')}}";
-    </script>
-    <title>Krixxi</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
+    </script>  
 
 </head>
 
