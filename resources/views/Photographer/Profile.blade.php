63482703e6c3953dd6ad0a32d0694e06f157d149
LAPTOP-G7028ELP\PIGFATUnchi <duetyunji@gmail.com>
2018-09-22 01:56:46 +0700

    update profile

--- resources/views/Photographer/Profile.blade.php
+++ resources/views/Photographer/Profile.blade.php
@@ -6,31 +6,31 @@
 @endsection
 @section('body')
 <section class="section-cover">
-    <div class="cover" style="margin-top:-5%;">
-      <img class="card-img-top cover-img" style="height: 400px;" src="..\images\profile\profile1\cover\cv1.jpg">
-    </div>
-</section>
+  <div class="" style="margin-top:-50%;">
+    <img class="card-img cover-img" src="{{asset('images/profile/profile1/cover/cv1.jpg')}}">
+  </div>
 
-<div class="container-fluid" style="padding-top:1%; padding-bottom:2%;">
-  <div class="col-sm-3 col-md-offset-1">
-    <img src="..\images\profile\profile1\p1.jpg" id="img" class="photographer-avatar">
+  <div class="container-fluid" style="padding-top:1%; padding-bottom:2%;">
+    <div class="col-sm-3 col-md-offset-1">
+      <img src="{{asset('images/profile/profile1/tommy.jpg')}}" id="img" class="photographer-avatar">
+    </div>
+    <div class="col-sm-7" style="padding-top:3%;">
+      <div class="font-topic-penname">TOMMY</div>
+      <div style="font-size:18px;"><span class="font-photograph">Detail : </span><span class="weight">สวัสดีค่ะดิฉันชื่อต๋อมหมี เป็นช่างภาพมืออาชีพ รักในการถ่ายภาพเป็นชีวิตจิตใจ หลงใหลในการกดชัตเตอร์ ขอบคุณทุกท่านที่เข้ามาดูผลงานของเรา หากรักในงานของเรา ให้เราเป็นส่วนหนึ่งในการสร้างความทรงจำให้กับทุกๆท่านนะคะ </span></div>
+      <ul class="list-unstyled"  style="font-size:18px;">
+        <li class="font-photograph glyphicon glyphicon-user"></li><span class="font-photograph">  Name     :  </span> <span class="weight">Thanita Khopengklang</span>
+        <br>
+        <li class="font-photograph glyphicon glyphicon-globe"></li><span class="font-photograph">  Location   :   </span> <span class="weight">ภาคตะวันออกเฉียงเหนือ </span>
+        <br>
+        <li class="font-photograph glyphicon glyphicon-camera" aria-hidden="true"></li><span class="font-photograph">  Half    :  </span> <span class="weight">2000</span></span>
+        <br>
+        <li class="font-photograph glyphicon glyphicon-camera" aria-hidden="true"></li><span class="font-photograph">  Full    :  </span>  <span class="weight">3000</span> </span>
+        <br>
+        <li class="" aria-hidden="true"></li><button class="btn-deal" data-toggle="modal" data-target="#myModal">Deal</button>
+      </ul>
+    </div>
   </div>
-  <div class="col-sm-7" style="padding-top:3%;">
-    <div class="font-topic-penname">TOMMY</div>
-    <div style="font-size:18px;"><span class="font-photograph">Detail : </span><span class="weight"><% photographer[0].description %></span></div>
-    <ul class="list-unstyled"  style="font-size:18px;">
-      <li class="font-photograph glyphicon glyphicon-user"></li><span class="font-photograph">  Name     :  </span> <span class="weight"><%  photographer[0].fullname %> </span>
-      <br>
-      <li class="font-photograph glyphicon glyphicon-globe"></li><span class="font-photograph">  Region    :   </span> <span class="weight"><% photographer[0].name %> </span>
-      <br>
-      <li class="font-photograph glyphicon glyphicon-camera" aria-hidden="true"></li><span class="font-photograph">  Half    :  </span> <span class="weight"><% photographer[0].parttime %></span></span>
-      <br>
-      <li class="font-photograph glyphicon glyphicon-camera" aria-hidden="true"></li><span class="font-photograph">  Full    :  </span>  <span class="weight"><% photographer[0].fulltime %></span> </span>
-      <br>
-      <li class="" aria-hidden="true"></li><button class="btn-deal" data-toggle="modal" data-target="#myModal">Deal</button>
-    </ul>
-  </div>
-</div>
+</section>
 
 @section('script')
 @endsection
