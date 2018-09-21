c47705fbe8005a2396b9cb2d17c089cdae6a81b8
jiidapa <jidapa.p@kkumail.com>
2018-09-21 22:36:23 +0700

    commit

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,9 +1,16 @@
 @extends('main')
 
+
+@section('import')
+<link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+@endsection
+
 @section('body')
-
-
-
+<section class="section-home">
+    <div class="cover-image">
+        
+    </div>
+</section>
 @section('script')
 <script src='{{asset('js/SearchController/SearchJs.js')}}'></script>
 @endsection
