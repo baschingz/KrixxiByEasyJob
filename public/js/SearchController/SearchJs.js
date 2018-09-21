a26dd38840b63f526a4cdacb22bf4d44923d6dc6
baschingz <36448854+baschingz@users.noreply.github.com>
2018-09-21 16:05:49 +0700

    commit

---
+++ public/js/SearchController/SearchJs.js
@@ -0,0 +1,22 @@
+app.controller('SearchController', function ($scope, $http) {
+    // get master data
+    $scope.init = function (page) {
+        page = page || null;
+        path = base_path + '/getselect';
+        // debugger;
+        $http.get(path).then(function (response) {
+            // debugger;
+            $scope.location = response.data[0];
+            $scope.cost = response.data[1];
+            $scope.service = response.data[2];
+            $scope.typeproject = response.data[3];
+        }, function (response) {
+            // debugger;
+            alert('fail');
+        });
+    }
+
+
+
+
+});
